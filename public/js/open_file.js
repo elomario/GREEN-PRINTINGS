30717a69324fe6a48148a1b1cfaf38570566dfa0
elomario <omarmater@yahoo.fr>
2016-04-09 18:19:17 +0300

    attributes on index.html
    Taking attributes

--- public/js/open_file.js
+++ public/js/open_file.js
@@ -1,20 +1,13 @@
-function readSingleFile(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var contents = e.target.result;
-    displayContents(contents);
-  };
-  reader.readAsText(file);
-}
-
-function displayContents(contents) {
-  var element = document.getElementById('file-content');
-  element.innerHTML = contents;
-}
-
-document.getElementById('file-input')
-  .addEventListener('change', readSingleFile, false);
+$(document).ready(function(){
+	$("#file-input").change(
+ 
+    function(e){
+		console.log($(this)[0].files[0]);
+        var file = $(this)[0].files[0];
+		$('.type').html(file.type);
+		$('.name').html(file.name);
+		$('.size').html(file.size);
+            console.log(file.name + file.type + ' (' + file.size + ').');       
+        return false;
+    });
+});
